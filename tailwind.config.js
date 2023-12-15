4e228c8e29143d55fdb160b6df10cf1d1faa7644
Victor Nadu <victorgnadu@gmail.com>
2023-12-15 14:53:05 +0400

    Fix dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./**/*.{html,js}',
   'node_modules/preline/dist/*.js',
-  'node_modules/@preline/overlay/*.js',
-],
+  'node_modules/@preline/overlay/*.js',],
   theme: {
     extend: {
       fontFamily: {
