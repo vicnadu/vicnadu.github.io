9f9d8de566d4c48db41bc5073962b9168b8f93a1
Victor Nadu <victorgnadu@gmail.com>
2024-12-07 01:22:59 -0500

    Components and pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				specialelite: ["Special Elite"],
+				ebgaramond: ["EB Garamond"],
+			},
+		  },
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
