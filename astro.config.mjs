dd1aa82bfeac710d9f4651ab9fdc7cb9553ae321
Victor Nadu <victorgnadu@gmail.com>
2024-12-08 20:54:22 -0500

    Pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://vicnadu.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  base: '/vicnadu.github.io/',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
